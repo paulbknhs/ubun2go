93db2a5d28ffc7d9c1230dcc0034d1ce0f83d0d9
paulbknhs <dev@paulbknhs.de>
2024-12-16 22:31:28 +0100

    fixed init.editor to core.editor

--- git-configure.sh
+++ git-configure.sh
@@ -3,6 +3,6 @@
 git config --global user.name "paulbknhs"
 git config --global user.email "dev@paulbknhs.de"
 git config --global init.defaultBranch "main"
-git config --global init.editor "vim"
+git config --global core.editor "nvim"
 
 git remote set-url origin --push git@github.com:paulbknhs/ubun2go.git
