e46c9a26cc9428b29e55076010ddf0ccd9083ac5
paulbknhs <dev@paulbknhs.de>
2024-12-25 18:00:16 +0100

    added ssh as upstream remote

--- install.sh
+++ install.sh
@@ -8,11 +8,13 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y git curl wget zsh gcc tree-sitter-cli lsd fzf
 
-mkdir -p $HOME/.config
+mkdir -p "$HOME"/.config
 
-[[ -d $NVIM ]] || git clone https://github.com/paulbknhs/nvim.git $CONFIG/nvim
-[[ -d $ZSH ]] || git clone https://github.com/paulbknhs/zsh.git $ZSH
-sudo ln -sf $ZSH/rc.zsh $HOME/.zshrc
-sudo ln -sf $ZSH/zsh_plugins.txt $HOME/.zsh_plugins.txt
+[[ -d $NVIM ]] || git clone https://github.com/paulbknhs/nvim.git "$CONFIG"/nvim
+[[ -d $ZSH ]] || git clone https://github.com/paulbknhs/zsh.git "$ZSH"
+sudo ln -sf "$ZSH"/rc.zsh "$HOME"/.zshrc
+sudo ln -sf "$ZSH"/zsh_plugins.txt "$HOME"/.zsh_plugins.txt
 
-chsh $USER
+cd "$NVIM" && git remote set-url origin --push git@github.com:paulbknhs/nvim.git
+cd "$ZSH" && git remote set-url origin --push git@github.com:paulbknhs/zsh.git
+chsh "$USER"
